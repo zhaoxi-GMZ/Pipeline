970136eabb9d3f6d763c0f9df7ffcf72643381f0
LuZhanXing <18330259691@qq.com>
2020-03-20 12:42:20 +0800

    shierdiansishier

--- test1test3p.sh
+++ test1test3p.sh
@@ -5,10 +5,12 @@ echo '----------------------------------------------start push------------------
 echo "当前分支："
 git branch
 
-  read -p "是否切换分支？Y|N：" branch_switch
+git_branch()
+{  read -p "是否切换分支？Y|N：" branch_switch
   if [ "$branch_switch" == "Y" ];then
        read -p '请输入要切换的分支的名称：' branch_name 
        git switch $branch_name
+       
        
   elif [  "$branch_switch" == "N" ];then
        git add .
@@ -18,11 +20,12 @@ git branch
       git pull
       echo '将本地推动到远程仓库'
        git push origin $branch_name
-      echo '您已成功推送'
+      echo '您已成功推送至远程仓库，改页面将在三秒后关闭......'
       sleep 3s
       exit
   else
     echo '请按提示输入：'
+    git_branch;
   fi
-
-
+}
+ git_branch;
